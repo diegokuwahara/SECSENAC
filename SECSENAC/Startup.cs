b577c6c4423257f0271e1e7af37b920d2833fa7e
Diego Kuwahara de Castro <diegookc@hotmail.com>
2018-10-17 21:12:23 -0300

    Adicionar arquivos de projeto.

---
+++ SECSENAC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SECSENAC.Startup))]
+namespace SECSENAC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
